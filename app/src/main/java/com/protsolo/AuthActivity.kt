2de9aa254d099087885a7b75db7871ff3943844b
oprotsenko <misterrygirl@gmail.com>
2021-09-05 23:28:12 +0300

    last update

--- app/src/main/java/com/protsolo/AuthActivity.kt
+++ app/src/main/java/com/protsolo/AuthActivity.kt
@@ -5,14 +5,32 @@ import android.os.Bundle
 import android.widget.Button
 
 import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        val emailData = findViewById<EditText>(R.id.editTextEmailAddressField)
+        emailData.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailData.text.toString()).matches())
+                    emailData.error = "Invalid email"
+            }
+
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
         setListeners()
     }
 
@@ -24,18 +42,9 @@ class AuthActivity : AppCompatActivity() {
     private fun register() {
         val intent = Intent(this, MainActivity::class.java)
         val emailData = findViewById<EditText>(R.id.editTextEmailAddressField)
-        if (!validator(emailData)) {
-            Toast.makeText(this, "e-mail not valid", Toast.LENGTH_SHORT).show()
-            setListeners()
-        }
+
         intent.putExtra("name", emailData.text.toString())
         startActivity(intent)
         finish()
-    }
-
-    private fun validator(emailData: EditText): Boolean {
-        if (emailData.text.isEmpty() || !emailData.text.matches(Regex("^[a-zA-Z0-9]+@[a-zA-Z0-9]+(.[a-zA-Z]{2,})$")))
-            return false
-        return true
     }
 }
