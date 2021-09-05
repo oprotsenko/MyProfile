802ea68a32da17ee84e8b08b92b0cdd63aa75805
oprotsenko <misterrygirl@gmail.com>
2021-09-06 00:07:22 +0300

    last update

--- app/src/main/java/com/protsolo/AuthActivity.kt
+++ app/src/main/java/com/protsolo/AuthActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 
 import android.content.Intent
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.EditText
-import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 
 
@@ -17,20 +14,11 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
         val emailData = findViewById<EditText>(R.id.editTextEmailAddressField)
-        emailData.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailData.text.toString()).matches())
-                    emailData.error = "Invalid email"
-            }
-
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-        })
+        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
+        emailData.doOnTextChanged { text, start, before, count ->
+            if (android.util.Patterns.EMAIL_ADDRESS.matcher(emailData.toString()).matches())
+                buttonRegister.isEnabled = true
+        }
         setListeners()
     }
 
@@ -42,7 +30,6 @@ class AuthActivity : AppCompatActivity() {
     private fun register() {
         val intent = Intent(this, MainActivity::class.java)
         val emailData = findViewById<EditText>(R.id.editTextEmailAddressField)
-
         intent.putExtra("name", emailData.text.toString())
         startActivity(intent)
         finish()
